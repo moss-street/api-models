f1c3cbea14b8fa368760d4b9ee112cc15a6b72fa
Justyn Lantz <jtlantz@amazon.com>
2025-01-06 22:25:22 -0800

    feat: more features
    add protobuf definitions for users and make build script compile to
    rust

--- build.rs
+++ build.rs
@@ -1,13 +1,14 @@
-use std::{env, error::Error, path::PathBuf};
+use anyhow::Result;
+use std::{env, path::PathBuf};
 
-fn main() -> Result<(), Box<dyn Error>> {
+fn main() -> Result<()> {
     let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
 
     tonic_build::configure()
-        .file_descriptor_set_path(out_dir.join("calculator_descriptor.bin"))
-        .compile_protos(&["proto/calculator.proto"], &["proto"])?;
+        .file_descriptor_set_path(out_dir.join("moss-street.bin"))
+        .compile_protos(&["protos/common.proto", "protos/users.proto"], &["protos"])?;
 
-    tonic_build::compile_protos("proto/calculator.proto")?;
-
+    tonic_build::compile_protos("protos/common.proto")?;
+    tonic_build::compile_protos("protos/users.proto")?;
     Ok(())
 }
