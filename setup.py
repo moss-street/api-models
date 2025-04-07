e8a3f8e527e7038618c04e497ba17b2fec8e454c
Justyn lantz <jtlantz@github.com>
2025-04-06 19:44:01 -0700

    feat: add api to get wallet balance

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="api_models",
-    version="0.1.3",
+    version="0.1.4",
     packages=find_packages(where="."),  # Only include generated files
     package_dir={"": "."},
     install_requires=[
